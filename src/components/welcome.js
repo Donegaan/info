3cbc87f8df0a39b215cc30a09f52906a5551669f
donegaan <donegaan@tcd.ie>
2018-05-11 14:21:39 +0100

    made welcome component

---
+++ src/components/welcome.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Welcome extends Component {
+  render() {
+    return (
+      <div className="Welcome section">
+        <h1 className="welcome-intro"><strong>Andrew Donegan</strong></h1>
+      </div>
+    );
+  }
+}
+export default Welcome;
