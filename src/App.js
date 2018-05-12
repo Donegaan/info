6ddc6f5a558e9b3a7d863415163b2c1435888b81
donegaan <donegaan@tcd.ie>
2018-05-12 22:25:36 +0100

    Added icons with links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+
 import './App.css';
 
 import Welcome from './components/welcome'
@@ -8,15 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React </h1>
-        </header>
-        <Welcome />
-        {/* <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          Test cool
-        </p> */}
+        <Welcome/>
       </div>
     );
   }
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import github from '../GitHub-Mark-64px.png'
+import linkedin from '../In-2C-66px-TM.png'
 class Welcome extends Component {
   render() {
     return (
       <div className="Welcome section">
         <h1 className="welcome-intro"><strong>Andrew Donegan</strong></h1>
+        <br />
+        <a href="https://github.com/Donegaan" >
+          <img src={github} hspace="20" />
+        </a>
+        < a href="https://www.linkedin.com/in/andrew-donegan-6a3514152/">
+          <img src={linkedin} />
+        </a>
       </div>
     );
   }
