5a4770e41def09b439268aec25f0f7bb70d8b7b0
donegaan <donegaan@tcd.ie>
2018-05-24 16:43:32 +0100

    Added icons, changed theme. Deployed to pages site

--- src/components/info.js
+++ src/components/info.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import github from '../images/GitHub-Mark-64px.png'
-import linkedin from '../images/linkedin.png'
+import github from '../images/GitHub-Mark-Light-64px.png'
+import linkedin from '../images/linkedinW.png'
 import cv from '../images/cv.png'
 
 class Welcome extends Component {
@@ -9,15 +9,17 @@ class Welcome extends Component {
       <div className="info-container">
         <h1>Andrew Donegan</h1>
         <br />
-        <a href="https://github.com/Donegaan" >
-          <img src={github} hspace="10" alt="github"/>
-        </a>
+        
         < a href="https://www.linkedin.com/in/andrew-donegan-6a3514152/">
           <img src={linkedin} hspace="10"alt="linkedin"/>
         </a>
-        <a href="https://donegaan.github.io/cv.pdf" target="_blank" rel="noopener noreferrer">
-          <img src={cv} hspace="10"alt="cv"/>
+        <a href="https://github.com/Donegaan" >
+          <img src={github} hspace="10" alt="github"/>
         </a>
+        <a href="https://donegaan.github.io/cv.pdf" target="_blank" rel="noopener noreferrer">
+          <img src={cv} hspace="10"alt="cv"/> {/*CV by unlimicon from the Noun Project*/}
+        </a>
+        
       </div>
     );
   }
