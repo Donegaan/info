36a01cfbf9a01f4e70d38cabfde16b0f129ea912
donegaan <donegaan@tcd.ie>
2018-06-01 20:20:17 -0500

    Added projects

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Info from './components/info'
+import Projects from './components/projects'
 
 class App extends Component {
   render() {
@@ -9,6 +10,8 @@ class App extends Component {
       <div className="App">
         <div className="container">
           <Info/>
+          <br/>
+          <Projects/>
         </div>
       </div>
     );
---
+++ src/components/projects.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div className="info-container">
+        <h1>Projects</h1>
+        <br />
+      </div>
+    );
+  }
+}
+export default Welcome;
